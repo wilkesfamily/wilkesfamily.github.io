b23b5f07e6e1594c4faaf6495de8ee7cae193f65
Steed <53194917+Squarerootofpi@users.noreply.github.com>
2023-06-20 18:56:53 -0600

    hi

--- script.js
+++ script.js
@@ -1,1 +1,5 @@
-alert("It is my birthday and everyone whose first letter in their name is in the alphabet has to bring me a present or I will make all your hard code,that you have worked so hard on, DISSAPEAR.")
+//alert("It is my birthday and everyone whose first letter in their name is in the alphabet has to bring me a present or I will make all your hard code,that you have worked so hard on, DISSAPEAR.")
+confetti();
+
+
+
