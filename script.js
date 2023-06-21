e1711fb07b8ebad2ef8398957ac399a4ac1470d2
Steed <53194917+Squarerootofpi@users.noreply.github.com>
2023-06-20 18:39:00 -0600

    leo

---
+++ script.js
@@ -0,0 +1,1 @@
+alert("It is my birthday and everyone whose first letter in their name is in the alphabet has to bring me a present or I will make all your hard code,that you have worked so hard on, DISSAPEAR.")
